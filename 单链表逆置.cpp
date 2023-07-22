8ea519f9e8e8d808924081d4959f5428086909df
林子健 <134626766+mumu1202@users.noreply.github.com>
2023-07-22 16:35:28 +0800

    Add files via upload
    为单链表类添加逆置方法，将表中所有元素进行逆置，如（1,2,3,4）逆置为（4,3,2,1），直接利用原表空间。

---
+++ 单链表逆置.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+template <typename DataType>
+struct Node
+{
+	DataType data;
+	Node<DataType>* next;
+};
+
+template <typename DataType>
+class LinkList
+{
+public:
+	LinkList(DataType a[], int n);
+	void PrintList();
+	void reverse();
+private:
+	Node<DataType>* first;
+};
+
+template <typename DataType>
+LinkList<DataType>::LinkList(DataType a[], int n)
+{
+	first = new Node<DataType>;
+	first->next = nullptr;
+	for (int i = 0;i < n;i++)
+	{
+		Node<DataType>* s = nullptr;
+
+		s = new Node<DataType>;s->data = a[i];
+
+		s->next = first->next;first->next = s;
+	}
+}
+
+template <typename DataType>
+void LinkList<DataType>::PrintList()
+{
+	Node<DataType>* p = first->next;
+	while (p != nullptr)
+	{
+		cout << p->data << " ";
+		p = p->next;
+	}
+}
+
+template <typename DataType>
+void LinkList<DataType>::reverse()
+{
+	Node<DataType>* p = first, * q = nullptr;
+	p = first->next;
+	first->next = nullptr;
+	q = p->next;
+	while (p)
+	{
+		p->next = first->next;
+		first->next = p;
+		p = q;
+		if (q)
+			q = q->next;
+	}
+}
+
+int main()
+{
+	int n, a[100] = {};
+	cin >> n;
+	if (n > 0)
+	{
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+	}
+	else
+	{
+		return 0;
+	}
+	LinkList<int> L{ a,n };
+	L.PrintList();
+
+	return 0;
+}
+
+
